dd30aec4e3a36225060aaee8ceff15010387be0a
khlebych <khlebych@yandex.ru>
2023-12-10 12:31:57 +0700

    Добавьте файлы проекта.

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/ping', methods=['GET'])
+def index():
+    return jsonify({'response': 'PPOONNGG'})
+
+
+if __name__ == '__maim__':
+    app.run(debug=True)
